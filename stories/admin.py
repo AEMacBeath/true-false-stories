06494d42fcf6ca0df028b8ce8f9a533bcd38de9f
AEMacBeath <alicemacbeath94@gmail.com>
2022-07-14 21:52:00 +0000

    Set up admin panel and connect Story and Comment models.

--- stories/admin.py
+++ stories/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Story, Comment
 
-# Register your models here.
+
+@admin.register(Story)
+class StoryAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'status', 'updated', 'author')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created')
+    summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'approved', 'created')
+    list_filter = ('name', 'approved', 'created')
+    actions = ['approve_comment']
+
+    def approve_comment(self, request, queryset):
+        queryset.update(approved=True)
