77582a9409870a5d66be4a87aa1ddfa404ef15a4
AEMacBeath <alicemacbeath94@gmail.com>
2022-07-15 16:28:44 +0000

    Create and link up html files for views.

--- stories/views.py
+++ stories/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Story
 
-# Create your views here.
+
+class StoryList(generic.ListView):
+    model = Story
+    gueryset = Story.objects.filter(status=1).order_by('-created')
+    template_name = 'index.html'
+    paginate_by = 5
